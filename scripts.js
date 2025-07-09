67b86303535d8cd2ad7a95669e4ebe6f0ee5b151
tibble93 <Tibble1456@gmail.com>
2025-07-09 19:01:41 -0400

    light toggle

--- scripts.js
+++ scripts.js
@@ -2,4 +2,8 @@ document.getElementById("modeToggle").addEventListener("click", function () {
   const sound = document.getElementById("toggleSound");
   sound.currentTime = 0; 
   sound.play();
+
+
+    document.body.classList.toggle("light-mode");
+
 });
