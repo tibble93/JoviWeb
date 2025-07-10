24fd77c90215b2135bb565d7e1fba42cbc760ea4
tibble93 <Tibble1456@gmail.com>
2025-07-09 22:23:20 -0400

    into message java

--- scripts.js
+++ scripts.js
@@ -5,10 +5,8 @@ document.getElementById("modeToggle").addEventListener("click", function () {
   sound.currentTime = 0; 
   sound.play();
 
-
     document.body.classList.toggle("light-mode");
-
-    
+  
   if (document.body.classList.contains("light-mode")) {
     icon.src = "images/darkmode.svg";
     icon.alt = "Moon icon";
@@ -26,3 +24,18 @@ if (document.body.classList.contains("light-mode")) {
   my_pic.src = "images/me_dark.png";
 }
 });
+
+const introText = "Welcome to my website. Thanks for stopping by and feel free to have a look around." 
+                  "You're more than welcome to leave a comment.";
+const introElement = document.getElementById("intro_Message");
+let charIndex = 0;
+
+function typeIntro() {
+  if (charIndex < introText.length) {
+    introElement.textContent += introText.charAt(charIndex);
+    charIndex++;
+    setTimeout(typeIntro, 40); // Speed: smaller = faster
+  }
+}
+
+window.addEventListener("DOMContentLoaded", typeIntro);
