afc2421f3020c634811402d130cbfa92d02bd65d
tibble93 <Tibble1456@gmail.com>
2025-07-09 21:50:14 -0400

    picture of me transition

--- scripts.js
+++ scripts.js
@@ -17,4 +17,12 @@ document.getElementById("modeToggle").addEventListener("click", function () {
     icon.alt = "Sun icon";
   }
 
+
+  const my_pic = document.getElementById("my_pic");
+
+if (document.body.classList.contains("light-mode")) {
+  my_pic.src = "images/me_light.png";
+} else {
+  my_pic.src = "images/me_dark.png";
+}
 });
