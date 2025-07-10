2e49454d4951cacc95628da5a9024f2c64b44690
tibble93 <Tibble1456@gmail.com>
2025-07-09 23:10:58 -0400

    fix toggle picture

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
+modeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
 
-
-const my_pic = document.getElementById("my_pic");
-
-if (document.body.classList.contains("light-mode")) {
-  my_pic.src = "images/me_light.png";
-} else {
-  my_pic.src = "images/me_dark.png";
-}
+  // Update picture
+  const my_pic = document.getElementById("my_pic");
+  my_pic.src = document.body.classList.contains("light-mode")
+    ? "images/me_light.png"
+    : "images/me_dark.png";
+});
 //-------------------------------------------------------------------------------------------------------//
 //-------------------------------------------------------------------------------------------------------//
 const introText = "Welcome to my website. Thanks for stopping by and feel free to have a look around." 
